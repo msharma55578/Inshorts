0b13b66bf36d48e037918547d86828ca98b058df
chander.mohan <chander.mohan@orchids.edu.in>
2021-08-03 19:28:11 +0530

    modifiy social media handles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,20 +6,14 @@ const Footer = () => {
     <div className="footer">
       <span className="name">
         Inshorts clone made by -{" "}
-        <a href="https://www.linkedin.com/in/piyush-eon" target="__blank">
-          Piyush Agarwal
+        <a href="https://www.linkedin.com/in/chander-mohan-b4a932134" target="__blank">
+          Chander Mohan
         </a>
       </span>
       <hr style={{ width: "90%" }} />
       <div className="iconContainer">
-        <a href="https://www.instagram.com/roadsidecoder/" target="__blank">
-          <i className="fab fa-instagram-square fa-2x"></i>
-        </a>
-        <a href="https://www.linkedin.com/in/piyush-eon" target="__blank">
+        <a href="https://www.linkedin.com/in/chander-mohan-b4a932134" target="__blank">
           <i className="fab fa-linkedin fa-2x"></i>
-        </a>
-        <a href="https://piyushjsx.netlify.app/" target="__blank">
-          <i className="fas fa-link fa-2x"></i>
         </a>
       </div>
     </div>
